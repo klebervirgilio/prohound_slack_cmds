e6010a6f9103cbeed7fd6277b72ee0836f3048c6
Kleber Correia <klebervirgilio@gmail.com>
2019-02-21 23:30:10 +0000

    expose data via api

--- lib/prohound_slack_cmds/router.ex
+++ lib/prohound_slack_cmds/router.ex
@@ -1,15 +1,22 @@
 defmodule ProhoundSlackCmds.Router do
   use Plug.Router
 
-  alias ProhoundSlackCmds.SmartCentre
+  alias ProhoundSlackCmds.SmartCentre.Model
 
   plug(Plug.Parsers, parsers: [:urlencoded])
   plug(Plug.Logger)
   plug(:match)
   plug(:dispatch)
 
-  post "/sc" do
-    IO.inspect(conn.params)
-    send_resp(conn, 200, "world")
+  get "/sc" do
+    {:ok, json} = Poison.encode(Model.all())
+
+    conn
+    |> put_resp_content_type("application/json")
+    |> send_resp(200, json)
+  end
+
+  match _ do
+    send_resp(conn, 404, "")
   end
 end
